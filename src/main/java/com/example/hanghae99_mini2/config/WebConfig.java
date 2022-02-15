532e7d4e7a91e8f3f7f02c13b90e2ffa12bd6cb7
paran21 <knospe1@naver.com>
2022-02-15 16:58:24 +0900

    logoutSuccessHandler 추가

--- src/main/java/com/example/hanghae99_mini2/config/WebConfig.java
+++ src/main/java/com/example/hanghae99_mini2/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ministudy.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("http://ministudy.s3-website.ap-northeast-2.amazonaws.com",
+                        "http://yjstudy.s3-website.ap-northeast-2.amazonaws.com/",
+                        "http://3.38.178.109/h2-console/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
--- src/main/java/com/example/hanghae99_mini2/security/WebSecurityConfig.java
+++ src/main/java/com/example/hanghae99_mini2/security/WebSecurityConfig.java
@@ -48,6 +48,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CustomLogoutSuccessHandler customLogoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler();
+    }
 
     @Override
     public void configure(WebSecurity web) {
@@ -74,7 +78,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 회원 관리 처리 API 전부를 login 없이 허용
                 .antMatchers("/user/**").permitAll()
                 // board, boards 관련 API login 없이 허용
-                .antMatchers("/boards").permitAll()
+                .antMatchers("/boards/**").permitAll()
 //                .antMatchers("/board/**").permitAll()
                 // 그 외 어떤 요청이든 '인증'
                 .anyRequest().permitAll()
@@ -101,8 +105,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout()
 // 로그아웃 요청 처리 URL
                 .logoutUrl("/user/logout")
-                .logoutSuccessUrl("/")
-                .permitAll();
+                .logoutSuccessHandler(customLogoutSuccessHandler());
     }
 
 //    @Bean
